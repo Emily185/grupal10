cd2866823daccb873dd9ee2f3a3b0b4c24c760b2
Josefina <josesilbermann@gmail.com>
2024-09-05 15:57:22 -0300

    for each arreglado + boton para probar

--- listado/js/almacenar.js
+++ listado/js/almacenar.js
@@ -15,15 +15,28 @@ function cargarItems(){
 function listaItems(items){
 let contenedorItems = document.getElementById('contenedor');
 contenedorItems.innerHTML = ""; //para que no se este repitiendo la lista contantemente cuando se actualiza
-items.forEach((item, () =>{ //creo un elemento de lista cada vez que se agrega un item al local storage
+items.forEach((item) =>{ //creo un elemento de lista cada vez que se agrega un item al local storage
     let li = document.createElement('li');
     li.textContent = item;
     li.className = 'list-group-item';
     contenedorItems.appendChild(li); //lo inserto en el contenedor con appendChild
-}))
+})
 
 }
 
+ //hacer que el boton agregue los elementos al local storage
+document.getElementById('agregar').addEventListener('click' ,()=>{
+    let input = document.getElementById('item');
+    let itemNuevo = input.value;
 
-
+    if(itemNuevo){
+        let items = JSON.parse(localStorage.getItem('items')) || [];
+        items.push(itemNuevo);
+        localStorage.setItem('items', JSON.stringify(items));
+        listaItems(items);
+    }
+    
 })
+
+
+}) 
