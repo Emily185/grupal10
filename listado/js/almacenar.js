8d892e8fff0e300e0a30dca79a1823719c403735
Josefina <josesilbermann@gmail.com>
2024-09-05 14:45:45 -0300

    Funcion que lista los objetos
    no se bien todavia si funciona ya que aun no esta activado el boton

--- listado/js/almacenar.js
+++ listado/js/almacenar.js
@@ -8,7 +8,22 @@ function cargarItems(){
     items = JSON.parse(itemsGuardados);
  }
  // aca se llamaria a una funcion q maneja la lista, la actualiza y tendriamos q hacer que no se multiplique Funcion(items);
+ listaItems(items);
 }
+
+//funcion para listar los items
+function listaItems(items){
+let contenedorItems = document.getElementById('contenedor');
+contenedorItems.innerHTML = ""; //para que no se este repitiendo la lista contantemente cuando se actualiza
+items.forEach((item, () =>{ //creo un elemento de lista cada vez que se agrega un item al local storage
+    let li = document.createElement('li');
+    li.textContent = item;
+    li.className = 'list-group-item';
+    contenedorItems.appendChild(li); //lo inserto en el contenedor con appendChild
+}))
+
+}
+
 
 
 })
